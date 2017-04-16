012f551aae86292db15f03f5a9cac8a05985d306
Martin Loitzl <martin@loitzl.com>
2017-04-16 10:27:24 +0200

    Constant Action Names

--- src/actions/courseAction.js
+++ src/actions/courseAction.js
@@ -1,3 +1,5 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
-    return { type: 'CREATE_COURSE', course }; // or { type: 'CREATE_COURSE', course } in ES6!
+    return { type: types.CREATE_COURSE, course }; // or { type: 'CREATE_COURSE', course } in ES6!
 }
